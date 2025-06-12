745837a56aeb4ce3e53cb3619a2db8923a8e9b13
Rajasekar Subramanian <rajassu2@cisco.com>
2025-06-11 23:28:24 -0500

    Add maven project

---
+++ src/main/java/com/example/codereviewbot/CodereviewbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.codereviewbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodereviewbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodereviewbotApplication.class, args);
+	}
+
+}
