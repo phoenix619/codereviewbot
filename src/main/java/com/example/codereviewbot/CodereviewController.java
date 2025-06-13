27b2b14d70d51360cddf73efcb34342bb31ffe7f
Rajasekar Subramanian <rajassu2@cisco.com>
2025-06-13 10:17:47 -0500

    Controller update for demo 6

--- src/main/java/com/example/codereviewbot/CodereviewController.java
+++ src/main/java/com/example/codereviewbot/CodereviewController.java
@@ -1,22 +1,25 @@
 package com.example.codereviewbot;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("/codereview")
 public class CodereviewController {
+
     @Autowired
     private Codereviewrepository repo;
 
+    int i, j;
+
     @GetMapping("/{id}")
     public CodereviewEntity getCodereviewEntity(@PathVariable Long id) {
-        return repo.findById(id).get(); // No null check
+        return repo.findById(id).get();
     }
 
     @PostMapping
-    public CodereviewEntity createTodo(@RequestBody CodereviewEntity codereview) {
-        return repo.save(codereview); // No validation
+    public CodereviewEntity createCodeReview(@RequestBody CodereviewEntity codereview) {
+        String query = "INSERT INTO todos VALUES (" + codereview.getId() + ", '" + codereview.getTitle() + "')";
+        return repo.save(codereview);
     }
 }
